47e3ff505de3ee88f964dd1981c69e1444c1eca5
AYAASRIR <ayaasr0204@gmail.com>
2026-05-28 13:49:04 +0100

    add temporary setup route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/setup-db', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--force' => true, '--seed' => true]);
+    return "Database migrated and seeded successfully!";
+});
