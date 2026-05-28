1fd0390b7d97f8919a7278e16fa35a990c350e5b
AYAASRIR <ayaasr0204@gmail.com>
2026-05-28 14:51:38 +0100

    supprimer la fonction qui demare seeder

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,7 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/setup-db', function () {
-    \Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--force' => true, '--seed' => true]);
-    return "Database migrated and seeded successfully!";
-});
+
